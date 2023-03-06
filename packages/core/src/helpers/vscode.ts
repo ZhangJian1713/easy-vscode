30d3c83480495df09912d6351dab65eafd0a355c
ZhangJian1713 <zhangjian1713@gmail.com>
2023-03-06 23:31:38 +0800

    remove unuse code

--- packages/core/src/helpers/vscode.ts
+++ packages/core/src/helpers/vscode.ts
@@ -1,10 +1,9 @@
 import * as vscode from 'vscode';
-import { EXTENSION_NAME } from '../constants'
 
-export async function saveDataToSettingJson(section: string, data: any, configurationTarget = true): Promise<void> {
-  await vscode.workspace.getConfiguration(EXTENSION_NAME).update(section, data, configurationTarget);
+export async function saveDataToSettingJson(configurationName: string, section: string, data: any, configurationTarget = true): Promise<void> {
+  await vscode.workspace.getConfiguration(configurationName).update(section, data, configurationTarget);
 }
 
-export function getDataFromSettingJson(section: string, defaultValue?: any): any {
-  return vscode.workspace.getConfiguration(EXTENSION_NAME).get(section, defaultValue);
+export function getDataFromSettingJson(configurationName: string, section: string, defaultValue?: any): any {
+  return vscode.workspace.getConfiguration(configurationName).get(section, defaultValue);
 }
